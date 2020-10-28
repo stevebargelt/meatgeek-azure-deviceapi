cde6616c8bf25a9d3d8b8a96c907ed6fd24543e4
Steve Bargelt <steve@bargelt.com>
2020-10-27 17:00:44 -0700

    Added missing using statement

--- src/MeatGeek.Device.Api/SmokerIoTGetTemps.cs
+++ src/MeatGeek.Device.Api/SmokerIoTGetTemps.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
