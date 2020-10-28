5aa1a676ad5acd73ede983f56a01b54b9be759bd
Steve Bargelt <steve@bargelt.com>
2020-10-28 04:39:24 -0700

    Getting rid of warnings from healthCheck

--- src/MeatGeek.Device.Api/HealthCheck.cs
+++ src/MeatGeek.Device.Api/HealthCheck.cs
@@ -17,22 +17,25 @@ namespace Inferno.Functions
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req, ILogger log)
         {
             log.LogInformation("Performing health check on Device API Function App.");
-
+            await Task.Yield();
+            
             // TODO: This is a very simple health check that ensures each configuration setting exists and has a value.
             // More thorough checks would validate each value against an expected format or by connecting to each service as required.
             // The function will return an HTTP status of 200 (OK) if all values contain non-zero strings.
             // If any are null or empty, the function will return an error, indicating which values are missing.
-
+            
             var RelayNamespace = Environment.GetEnvironmentVariable("RelayNamespace", EnvironmentVariableTarget.Process);
             var RelayConnectionName = Environment.GetEnvironmentVariable("RelayConnectionName", EnvironmentVariableTarget.Process);
             var RelayKeyName = Environment.GetEnvironmentVariable("RelayKeyName", EnvironmentVariableTarget.Process);
             var RelayKey = Environment.GetEnvironmentVariable("RelayKey", EnvironmentVariableTarget.Process);
+            var IoTServiceConnection = Environment.GetEnvironmentVariable("InfernoIoTServiceConnection", EnvironmentVariableTarget.Process);
 
             var variableList = new List<string>();
             if (string.IsNullOrWhiteSpace(RelayNamespace)) variableList.Add("RelayNamespace");
             if (string.IsNullOrWhiteSpace(RelayConnectionName)) variableList.Add("RelayConnectionName");
             if (string.IsNullOrWhiteSpace(RelayKeyName)) variableList.Add("RelayKeyName");
             if (string.IsNullOrWhiteSpace(RelayKey)) variableList.Add("RelayKey");
+            if (string.IsNullOrWhiteSpace(IoTServiceConnection)) variableList.Add("IoTServiceConnection");
 
             if (variableList.Count > 0)
             {
