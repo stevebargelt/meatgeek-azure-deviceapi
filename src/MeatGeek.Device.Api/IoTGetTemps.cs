90eed6eb11b2914210f6533d78fcc497dbacb398
Steve Bargelt <steve@bargelt.com>
2020-11-03 12:50:15 -0800

    Remove MEatGeek IoT for now

--- src/MeatGeek.Device.Api/IoTGetTemps.cs
+++ src/MeatGeek.Device.Api/IoTGetTemps.cs
@@ -33,15 +33,15 @@ namespace Inferno.Functions
             log.LogInformation("Response status: {0}, payload: {1}", response.Status, response.GetPayloadAsJson());
             log.LogInformation("END: Inferno IoT");
 
-            log.LogInformation("START: MeatGeek IoT");
-            MeatGeekServiceConnectionString = Environment.GetEnvironmentVariable("MeatGeekIoTServiceConnection", EnvironmentVariableTarget.Process);
-            MeatGeekIoTHubServiceClient = ServiceClient.CreateFromConnectionString(MeatGeekServiceConnectionString);
-            var methodInvocationMeatGeek = new CloudToDeviceMethod("SmokerGetTemps") { ResponseTimeout = TimeSpan.FromSeconds(30) };
-            // Invoke the direct method asynchronously and get the response from the device.
-            var response1 = await MeatGeekIoTHubServiceClient.InvokeDeviceMethodAsync("meatgeek1", "CSharpModule", methodInvocation);
-            log.LogInformation("Response status: {0}, payload: {1}", response1.Status, response1.GetPayloadAsJson());
+            // log.LogInformation("START: MeatGeek IoT");
+            // MeatGeekServiceConnectionString = Environment.GetEnvironmentVariable("MeatGeekIoTServiceConnection", EnvironmentVariableTarget.Process);
+            // MeatGeekIoTHubServiceClient = ServiceClient.CreateFromConnectionString(MeatGeekServiceConnectionString);
+            // var methodInvocationMeatGeek = new CloudToDeviceMethod("SmokerGetTemps") { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            // // Invoke the direct method asynchronously and get the response from the device.
+            // var response1 = await MeatGeekIoTHubServiceClient.InvokeDeviceMethodAsync("meatgeek1", "CSharpModule", methodInvocation);
+            // log.LogInformation("Response status: {0}, payload: {1}", response1.Status, response1.GetPayloadAsJson());
             
-            log.LogInformation("END: MeatGeek IoT");
+            // log.LogInformation("END: MeatGeek IoT");
 
             return new ObjectResult(response.GetPayloadAsJson());
         }
